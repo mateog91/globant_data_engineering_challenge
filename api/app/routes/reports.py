fe1eb80ead94b77a4918f5807e6b9d1dcc6de9e8
Mateo <mateog91@gmail.com>
2023-08-18 15:02:46 -0500

    ADD: second query report

--- api/app/routes/reports.py
+++ api/app/routes/reports.py
@@ -31,6 +31,39 @@ WHERE EXTRACT(year FROM he.datetime) = 2021
 GROUP BY d.department, j.job;
 """
 
+QUERY2 = """
+WITH department_stats AS (
+    SELECT
+        d.id,
+        d.department AS department,
+        COUNT(he.id) AS hired
+    FROM
+        departments d
+    LEFT JOIN
+        hired_employees he ON d.id = he.department_id
+    WHERE
+        EXTRACT(year FROM he.datetime) = 2021
+    GROUP BY
+        d.id, d.department
+),
+avg_hired AS (
+    SELECT
+        AVG(hired) AS mean_hired
+    FROM
+        department_stats
+)
+SELECT
+    ds.id,
+    ds.department,
+    ds.hired
+FROM
+    department_stats ds
+JOIN
+    avg_hired ah ON ds.hired > ah.mean_hired
+ORDER BY
+    ds.hired DESC;
+"""
+
 
 @router.get("/")
 def hired_employees_by_department_and_job(db: Session = Depends(get_db)
@@ -40,7 +73,20 @@ def hired_employees_by_department_and_job(db: Session = Depends(get_db)
     """
     columns = ["department", "job", "Q1", "Q2", "Q3", "Q4"]
     result = db.execute(sa.text(QUERY))
-    
+
+    result2 = [dict(zip(columns, row)) for row in result]
+
+    return result2
+
+@router.get("/hired_employees_by_department_over_average")
+def hired_employees_by_department_over_average(db: Session = Depends(get_db)
+                                               ):
+    """
+    Get hired employees by department and job
+    """
+    columns = ["id", "department", "hired"]
+    result = db.execute(sa.text(QUERY))
+
     result2 = [dict(zip(columns, row)) for row in result]
 
     return result2
